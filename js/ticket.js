8a1278caf97109d4a75e8734fc30a08050a533a0
Your Name <you@example.com>
2023-06-28 01:55:24 +0200

    små ting

--- js/ticket.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var matchDropdown = document.getElementById("matchDropdown");
-  
-    matchDropdown.addEventListener("change", function() {
-      var selectedValue = matchDropdown.value;
-      console.log("Selected value: " + selectedValue);
-    });
-  });
-  
