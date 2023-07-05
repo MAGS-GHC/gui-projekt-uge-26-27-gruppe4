0ccd06b1e049c2d122acca86068f09d59dc4a7f7
cbc1000 <127729826+cbc1000@users.noreply.github.com>
2023-07-05 10:39:50 +0200

    season DONE (mangler fetch)

---
+++ js/season.js
@@ -0,0 +1,28 @@
+var grid = document.querySelector(".grid-container");
+
+function seeTicket() {
+  grid.innerHTML += `
+<div class="grid-wrapper">
+    <div class="grid-itemName">
+        Calle Callesen <br>
+        88888888
+    </div>
+    <div class="grid-itemMail">
+        viborg@viborg.dk
+    </div>
+    <div class="grid-itemIMG">
+        <img src="../assets/images/teamphoto.jpg" alt="">
+    </div>
+    <div class="grid-itemSeasonID">
+        SæsonkortNr.
+    </div>
+    <div class="grid-itemSectionNR">
+        SektionNr.
+    </div>
+</div>   
+`;
+}
+
+window.onload = function () {
+  seeTicket();
+};
