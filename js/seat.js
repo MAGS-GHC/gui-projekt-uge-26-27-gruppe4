a3184ca9b63b7c92026df67663f93562784d88db
Your Name <you@example.com>
2023-07-06 17:01:28 +0200

    seat dynamisk + sæde id'er til LS

--- js/seat.js
+++ js/seat.js
@@ -4,38 +4,110 @@ function generateSeats() {
   let seatContainer = document.getElementById("seatContainer");
   seatContainer.innerHTML = "";
 
-  // Retrieve the seat data from localStorage
   const seatData = JSON.parse(localStorage.getItem("selectedMatch"));
-  
-  console.log(seatData)
 
-
+  console.log(seatData);
 
   if (!seatData) {
     console.log("Seat data not found in localStorage.");
     return;
   }
 
-  if(selectedSection === seatData.sections[0].sectionName){
+  let sectionCount = 0;
 
+  if (selectedSection === "M") {
+    console.log("her printer vi 600 billeter");
+    printSeats(seatData,sectionCount);
+    saveSeats(seatData,sectionCount);
+  }
+
+  if (selectedSection === "L") {
+    let sectionCount = 1;
+    console.log("her printer vi 600 billeter");
+    printSeats(seatData,sectionCount);
+    saveSeats(seatData,sectionCount);
+  }
+
+  if (selectedSection === "K") {
+    let sectionCount = 2;
+    console.log("her printer vi 230 billeter");
+    printSeats(seatData,sectionCount);
+    saveSeats(seatData,sectionCount);
+  }
+
+  if (selectedSection === "I") {
+    let sectionCount = 3;
+    console.log("her printer vi 150 billeter");
+    printSeats(seatData,sectionCount);
+    saveSeats(seatData,sectionCount);
+  }
+
+  if (selectedSection === "J") {
+    let sectionCount = 4;
+    console.log("her printer vi 150 billeter");
+    printSeats(seatData,sectionCount);
+    saveSeats(seatData,sectionCount);
+  }
+
+  if (selectedSection === "H") {
+    let sectionCount = 5;
+    console.log("her printer vi 150 billeter");
+    printSeats(seatData,sectionCount);
+    saveSeats(seatData,sectionCount);
+  }
+
+  if (selectedSection === "G") {
+    let sectionCount = 6;
+    console.log("her printer vi 280 billeter");
+    printSeats(seatData,sectionCount);
+    saveSeats(seatData,sectionCount);
+  }
+
+  if (selectedSection === "M Fan") {
+    console.log("her printer vi 300 billeter");
+    let sectionCount = 7;
+    printSeats(seatData,sectionCount);
+    saveSeats(seatData,sectionCount);
   }
 
 
-let totalSeats = seatData.sections[0].tickets.length;
+function saveSeats(seatData, sectionCount){
+  const seats = document.querySelectorAll(".seat");
+  let selectedSeatIds = [];
 
+  for (let x = 0; x < seats.length; x++) {
+    seats[x].addEventListener("click", () => {
+      console.log(x);
+      console.log(seatData.sections[sectionCount].tickets[x].id);
 
-let rows = 0;
-for(let q = 0; q < seatData.sections[0].tickets.length; q++){
-  console.log(seatData.sections[0].tickets[q].seat.row)
-  rows = seatData.sections[0].tickets[q].seat.row
+      console.log(seatData.sections[sectionCount].tickets[x].id);
+      console.log(
+        "denne blev klikket" + seatData.sections[sectionCount].tickets[x].id
+      );
+
+      const clickedSeatId = seatData.sections[sectionCount].tickets[x].id;
+      selectedSeatIds.push(clickedSeatId);
+      localStorage.setItem("selectedSeatIds", JSON.stringify(selectedSeatIds));
+    });
+  }
+}
 }
 
 
-let seatsPerRow = Math.ceil(totalSeats / rows);
+function printSeats(seatData, sectionCount){
 
+  console.log(sectionCount)
+  let totalSeats = seatData.sections[sectionCount].tickets.length;
 
+  let rows = 0;
+  for (let q = 0; q < seatData.sections[sectionCount].tickets.length; q++) {
+    console.log(seatData.sections[sectionCount].tickets[q].seat.row);
+    rows = seatData.sections[sectionCount].tickets[q].seat.row;
+  }
 
+  let seatsPerRow = Math.ceil(totalSeats / rows);
   let seatCount = 0;
+
   for (let i = 1; i <= rows; i++) {
     let row = document.createElement("div");
     row.classList.add("row");
@@ -43,27 +115,12 @@ let seatsPerRow = Math.ceil(totalSeats / rows);
     let seatsInRow = Math.min(totalSeats - seatCount, seatsPerRow);
     for (let j = 1; j <= seatsInRow; j++) {
       let seat = document.createElement("div");
-      seat.classList.add(`seat${seatCount}`);
+      seat.classList.add(`seat`);
 
       row.appendChild(seat);
-     seatCount++;
-   }
-
+      seatCount++;
+    }
     seatContainer.appendChild(row);
   }
 
-  console.log("Seats printed: " + seatCount);
-  
-  const seats = document.querySelectorAll('.seat');
-  
-  console.log(seatData.sections[0].tickets[2].id)
-  
-  console.log(seats)
-  
-  for(let i = 0; i < seatID.length; i++){
-    
-    seatID[i].addEventListener("click", () =>{
-      console.log("denne blev klikket" + seatData.sections[0].tickets[i].id )
-    })
-  }
 }
