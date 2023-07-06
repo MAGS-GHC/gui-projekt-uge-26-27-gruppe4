bf8ac613bf601d567c029de94f1172ff231d026e
KevinNielsen00 <122385622+KevinNielsen00@users.noreply.github.com>
2023-07-06 11:41:01 +0200

    updated seat.js to generate the seat with data from db

--- js/seat.js
+++ js/seat.js
@@ -1,46 +1,53 @@
 function generateSeats() {
-    var sectionSelect = document.getElementById("sectionSelect");
-    var selectedSection = sectionSelect.value;
-    var seatContainer = document.getElementById("seatContainer");
-    seatContainer.innerHTML = "";
+  let sectionSelect = document.getElementById("sectionSelect");
+  let selectedSection = sectionSelect.value;
+  let seatContainer = document.getElementById("seatContainer");
+  seatContainer.innerHTML = "";
 
-    var sectionInfo = {
-      "M": { totalSeats: 600, rows: 18 },
-      "L": { totalSeats: 600, rows: 18 },
-      "K": { totalSeats: 230, rows: 18 },
-      "J Nedre": { totalSeats: 150, rows: 6 },
-      "I Nedre": { totalSeats: 150, rows: 6 },
-      "H Nedre": { totalSeats: 150, rows: 6 },
-      "G": { totalSeats: 280, rows: 18 }
-    };
+  // Fetch the seat data from the database
+  fetch("https://helloworld-pxy7m5opzq-lz.a.run.app/matches")
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Error fetching seat data");
+      }
+    })
+    .then(function (seatData) {
+      // Find the selected section in the retrieved seat data here
+      let sectionData = seatData.find(function (seat) {
+        return seat.section === selectedSection;
+      });
 
-    var sectionData = sectionInfo[selectedSection];
-    if (!sectionData) {
-      console.log("Invalid section selected.");
-      return;
-    }
-
-    var totalSeats = sectionData.totalSeats;
-    var rows = sectionData.rows;
-    var seatsPerRow = Math.ceil(totalSeats / rows);
-
-
-    var seatCount = 0;
-    for (var i = 1; i <= rows; i++) {
-      var row = document.createElement("div");
-      row.classList.add("row");
-
-      var seatsInRow = Math.min(totalSeats - seatCount, seatsPerRow);
-      for (var j = 1; j <= seatsInRow; j++) {
-        var seat = document.createElement("div");
-        seat.classList.add("seat");
-
-        row.appendChild(seat);
-        seatCount++;
+      if (!sectionData) {
+        console.log("Invalid section selected.");
+        return;
       }
 
-      seatContainer.appendChild(row);
-    }
+      let totalSeats = sectionData.totalSeats;
+      let rows = sectionData.rows;
+      let seatsPerRow = Math.ceil(totalSeats / rows);
 
-    console.log("Seats printed: " + seatCount);
-  }
+      let seatCount = 0;
+      for (let i = 1; i <= rows; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+
+        let seatsInRow = Math.min(totalSeats - seatCount, seatsPerRow);
+        for (let j = 1; j <= seatsInRow; j++) {
+          let seat = document.createElement("div");
+          seat.classList.add("seat");
+
+          row.appendChild(seat);
+          seatCount++;
+        }
+
+        seatContainer.appendChild(row);
+      }
+
+      console.log("Seats printed: " + seatCount);
+    })
+    .catch(function (error) {
+      console.log("Error: " + error.message);
+    });
+}
--- js/seatselect.js
+++ js/seatselect.js
@@ -1,60 +1,55 @@
 const seats = document.querySelectorAll('.row .seat');
+const seatingContainer = document.querySelector('.seating-container');
+const svgStadium = document.querySelector('.svg-stadium');
+const container = document.querySelector('.container');
 
 // Function that updates the count of booked seats and saves data to local storage
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
-
   const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
-
-  // Count the number of selected seats
   const selectedSeatsCount = selectedSeats.length;
-
   // Update the UI to display the count of selected seats
-  //document.getElementById('count').innerText = selectedSeatsCount;
+  // document.getElementById('count').innerText = selectedSeatsCount;
 }
-
 
 // Function to handle seat selection
 function handleSeatSelection(e) {
-  // Check if the clicked seat is a seat and not already booked
   if (e.target.classList.contains('seat') && !e.target.classList.contains('booked')) {
-    // Toggle the "selected" class for the clicked seat
     e.target.classList.toggle('selected');
-
-    // Call the function to update the count of selected seats
     updateSelectedCount();
   }
 }
+
 // Add event listener for seat selection
-document.querySelector('.seatContainer').addEventListener('click', handleSeatSelection);
-const seatingContainer = document.querySelector('.seating-container');
-const svgStadium = document.querySelector('.svg-stadium');
+document.querySelector('#seatContainer').addEventListener('click', handleSeatSelection);
 
 // Function to show the selected section and their seats
 function showSection(section) {
-  // Hide the svg-stadium
   svgStadium.style.display = 'none';
-
-  // Hide all sections
   const sections = document.querySelectorAll('.section');
   sections.forEach((section) => {
     section.style.display = 'none';
   });
-
   const selectedSection = document.getElementById(`section${section}`);
   selectedSection.style.display = 'block';
-
   seatingContainer.classList.remove('hidden');
   container.classList.remove('hidden');
-
   const currentSection = document.querySelector('.currentSection');
-
   const sectionText = document.querySelector('#sectionText');
-
   sectionText.textContent = `Section ${section}`;
-
   updateSelectedCount();
+}
+
+// Event handler for section image click
+function handleSectionClick(event) {
+  const sectionId = event.target.getAttribute('id');
+  if (sectionId === 'svg-section-MF') {
+    console.log('Perform a different action for Section-MF');
+  } else {
+    const section = sectionId.replace('svg-section-', '');
+    showSection(section);
+  }
 }
 
 // Add event listeners to the images
@@ -63,66 +58,16 @@ sectionImages.forEach((image) => {
   image.addEventListener('click', handleSectionClick);
 });
 
-// Event handler for section image click
-function handleSectionClick(event) {
-  const sectionId = event.target.getAttribute('id');
-  
-  if (sectionId === 'svg-section-MF') {
-    // Handle Section-MF differently
-    // Example: window.location.href = 'https://example.com';
-    console.log('Perform a different action for Section-MF');
-  } else {
-    // Handle other sections
-    const section = sectionId.replace('svg-section-', '');
-    showSection(section);
-  }
-}
-
 // Function to populate the UI with booked and selected seats
 function populateUI() {
   const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
-
-  // Check for selected seats in local storage
   if (selectedSeats !== null && selectedSeats.length > 0) {
-    // Iterate over the seats and add the "selected" class to the selected seats
     seats.forEach((seat, index) => {
       if (selectedSeats.indexOf(index) > -1) {
         seat.classList.add('selected');
       }
     });
   }
-}
-
-// Function to do the seat selection
-function handleSeatSelection(e) {
-  // Check if the clicked seat is a seat and not already booked
-  if (e.target.classList.contains('seat') && !e.target.classList.contains('booked')) {
-    // make the seats you click the selected class
-    e.target.classList.toggle('selected');
-
-    // call the funktion that saves to local storage
-    updateSelectedCount();
-  }
-}
-
-function showSection(section) {
-  // Hide all sections
-  const sections = document.getElementsByClassName('section');
-  for (let i = 0; i < sections.length; i++) {
-    sections[i].style.display = 'none';
-  }
-  
-  // Show the selected section
-  const selectedSection = document.getElementById(`section${section}`);
-  selectedSection.style.display = 'block';
-
-  // Get the currentSection div element
-  var sectionM = document.querySelector('.currentSection');
-
-  // Get the <p> element inside currentSection
-  var sectionText = document.querySelector('#sectionText');
-
-  sectionText.textContent = 'Section ' + section;
 }
 
 populateUI();
