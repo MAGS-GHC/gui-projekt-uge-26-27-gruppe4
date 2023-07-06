162b3cae28f4b9831b8dc3b86a8dee085267b090
Your Name <you@example.com>
2023-07-06 12:22:04 +0200

    gemmer login i localstorage

--- main.js
+++ main.js
@@ -1,49 +1,48 @@
-
 function loginUser(event) {
-    event.preventDefault();
-  
-    const email = document.getElementById('loginEmail').value;
-    const password = document.getElementById('loginPassword').value;
-  
-    const loginData = {
-      email,
-      password
-    };
-  
-    console.log(loginData);
-  
-    fetch('https://helloworld-pxy7m5opzq-lz.a.run.app/usersVFF/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(loginData)
+  event.preventDefault();
+
+  const email = document.getElementById('loginEmail').value;
+  const password = document.getElementById('loginPassword').value;
+
+  const loginData = {
+    email,
+    password
+  };
+
+  console.log(loginData);
+
+  fetch('https://helloworld-pxy7m5opzq-lz.a.run.app/usersVFF/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(loginData)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      console.log(data.message);
+      console.log('Login Response:', data);
+
+      if (data.message === 'Login successful') {
+        fetch('https://helloworld-pxy7m5opzq-lz.a.run.app/usersVFF')
+          .then(response => response.json())
+          .then(users => {
+            const user = users.find(user => user.email === email);
+            if (user) {
+              // Save the logged-in user in local storage
+              localStorage.setItem('loggedInUser', JSON.stringify(user));
+              window.location.href = 'menu';
+            } else {
+              console.error('User not found in the database');
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+      }
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        console.log(data.message);
-        console.log('Login Response:', data);
-  
-        if (data.message === 'Login successful') {
-          fetch('https://helloworld-pxy7m5opzq-lz.a.run.app/usersVFF')
-            .then(response => response.json())
-            .then(users => {
-              const user = users.find(user => user.email === email);
-              if (user) {
-                window.location.href = '/menu';
-              } else {
-                console.error('User not found in the database');
-              }
-            })
-            .catch(error => {
-              console.error('Error:', error);
-  
-            });
-        }
-      })
-      .catch(error => {
-        console.error('Error logging in:', error);
-      });
-  }
-  
+    .catch(error => {
+      console.error('Error logging in:', error);
+    });
+}
